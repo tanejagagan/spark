f8c1eaa7b9e70e7a8e5c86044a898d6a7dd3202d
Gagan Personal <gagan-personal@Gagans-MacBook-Pro.local>
2020-02-13 17:23:16 -0800

    UnsafeJsonEncoder to support Key Value encoding

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/json/UnsafeJsonEncoder.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/json/UnsafeJsonEncoder.scala
@@ -16,16 +16,20 @@
  */
 package org.apache.spark.sql.catalyst.json
 
+import java.io.{ByteArrayOutputStream, OutputStreamWriter}
+import java.util.{Map => JMap}
+
 import com.fasterxml.jackson.core.{JsonFactory, JsonParser}
 
-import org.apache.spark.sql.catalyst.expressions.{UnsafeProjection, UnsafeRow}
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.expressions.{AttributeReference, UnsafeProjection, UnsafeRow}
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.unsafe.types.UTF8String
 
 
 object UnsafeJsonEncoder {
 
-  class Encoder(schemaStr : String,
+  private class Encoder(schemaStr : String,
                 jParser : JacksonParser,
                 unsafeProjection: UnsafeProjection) extends (Array[Byte] => Array[Byte]) {
     override def apply(bytes: Array[Byte]): Array[Byte] = {
@@ -40,11 +44,101 @@ object UnsafeJsonEncoder {
     }
   }
 
-  val builder : String => Array[Byte] => Array[Byte] = { schemaStr =>
+  private class KeyValueEncoder(schemaStr : String,
+                        keyColumns : Seq[String],
+                        jParser: JacksonParser,
+                        keyUnsafeProjection: Option[UnsafeProjection],
+                        valueUnsafeProjection : Option[UnsafeProjection])
+    extends (Array[Byte] => (Array[Byte], Array[Byte])) {
+
+    override def apply(bytes: Array[Byte]): (Array[Byte], Array[Byte]) = {
+      val rows = jParser.parse[Array[Byte]](bytes, byteArrayParser, b => UTF8String.fromBytes(b) )
+      val kRow = extractRow(rows, keyUnsafeProjection, false)
+      val vRow = extractRow(rows, valueUnsafeProjection, true)
+      (kRow, vRow)
+    }
+
+
+    // If all Values are null then value should be set to null
+    // This will have kafka reclaim during compaction
+    private def extractRow(rows: Seq[InternalRow],
+                           oProjection: Option[UnsafeProjection],
+                           overrideAllNull : Boolean ): Array[Byte] = {
+      oProjection.map { projection =>
+        val rRow = projection.apply(rows.head)
+        if(overrideAllNull && allNull(rRow)) {
+          null
+        } else {
+          UnsafeRow.writeExternal(rRow)
+        }
+      }.getOrElse(null)
+    }
+
+    private def allNull(row: UnsafeRow): Boolean = {
+      if (!row.anyNull()) {
+        return false
+      }
+      var i = 0
+      val fields = row.numFields()
+      while (i < fields) {
+        if (!row.isNullAt(i)) {
+          return false
+        }
+        i += 1
+      }
+      return true
+    }
+
+    override def toString(): String = {
+      val params = jParser.options.parameters.mkString(",")
+      val columns = keyColumns.map(c => s"$c").mkString(",")
+      s"""{"schemaStr" : "$schemaStr", "keyColumns" : [${columns.mkString(",")}],
+         | "parserOptions" : "$params"}""".stripMargin
+    }
+  }
+
+  val encoderFor : String => Array[Byte] => Array[Byte] = { schemaStr =>
     val schema = StructType.fromDDL(schemaStr)
     val jParser = new JacksonParser(schema, new JSONOptions(Map(), "UTC"))
     val unsafeProjection = UnsafeProjection.create(schema)
     new Encoder(schemaStr, jParser, unsafeProjection)
+  }
+
+  val decoderFor : String =>
+    InternalRow => Array[Byte] = { schemaStr =>
+    val schema = StructType.fromDDL(schemaStr)
+    val fn: InternalRow => Array[Byte] = { row =>
+      val stream = new ByteArrayOutputStream()
+      val writer =
+        new OutputStreamWriter(stream)
+      val generator = new JacksonGenerator(schema, writer,
+        new JSONOptions(Map(), "UTC"))
+      generator.write(row)
+      stream.toByteArray
+    }
+    fn
+  }
+
+  val keyValueEncoderFor : (String,
+    Array[String]) => Array[Byte] => (Array[Byte], Array[Byte]) = { (schemaStr, keyColumns) =>
+    val schema = StructType.fromDDL(schemaStr)
+    val jParser = new JacksonParser(schema, new JSONOptions(Map(), "UTC"))
+    val inputSchemaAttributes = schema.fields.map(f => new AttributeReference(f.name, f.dataType)())
+    val inputSchemaAttributesMap = inputSchemaAttributes.map( a => a.name -> a).toMap
+    val valueSchemaFields = schema.fields.filterNot(c => keyColumns.contains(c.name))
+    val keyAttributes = keyColumns.map( c => inputSchemaAttributesMap(c))
+    val valueAttributes = valueSchemaFields.map( f => inputSchemaAttributesMap(f.name))
+    val keyUnsafeProjection = if (keyAttributes.nonEmpty) {
+      Some(UnsafeProjection.create(keyAttributes, inputSchemaAttributes))
+    } else {
+      None
+    }
+    val valueUnsafeProjection = if (valueAttributes.nonEmpty) {
+      Some(UnsafeProjection.create(valueAttributes, inputSchemaAttributes))
+    } else {
+      None
+    }
+    new KeyValueEncoder(schemaStr, keyColumns, jParser, keyUnsafeProjection, valueUnsafeProjection)
   }
 
   def byteArrayParser(jsonFactory: JsonFactory, record: Array[Byte]): JsonParser = {
--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/json/UnsafeJsonEncoderSpec.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/json/UnsafeJsonEncoderSpec.scala
@@ -24,18 +24,116 @@ import org.apache.spark.sql.catalyst.expressions.UnsafeRow
 
 class UnsafeJsonEncoderSpec extends WordSpec {
   "EncoderBuilder" should {
-    "encode json correctly" in {
-      val schemaStr = "intVal int, stringVal string"
-      val json = """{"intVal": 10, "stringVal": "10"}"""
-      val encoder = UnsafeJsonEncoder.builder(schemaStr)
-      val encoder2 = UnsafeJsonEncoder.builder(schemaStr + ", floatVal float")
-
+    "Encode json correctly" in {
+      val schemaStr = "intVal int, stringVal string, longVal bigint"
+      val json = """{"intVal": 10, "stringVal": "10", "longVal" : 100 }"""
+      val encoder = UnsafeJsonEncoder.encoderFor(schemaStr)
+      val encoder2 = UnsafeJsonEncoder.encoderFor(schemaStr + ", floatVal float")
       val start = System.currentTimeMillis()
       var i = 0;
-      while (i < 100000) {
+      while (i < 10) {
         val byteArray = encoder(json.getBytes())
         val unsafeRow = UnsafeRow.readExternal(byteArray)
-        i+=1
+        assert(unsafeRow.getInt(0) == 10)
+        assert(unsafeRow.getString(1) == "10")
+        assert(unsafeRow.getLong(2) == 100L)
+        i += 1
+      }
+      val end = System.currentTimeMillis()
+    }
+
+    "Encode json correctly with keys" in {
+      val schemaStr = "intVal int, stringVal string, longVal bigint"
+      val keyColumns = Array("intVal")
+      val json = """{"intVal": 10, "stringVal": "10", "longVal" : 100 }"""
+      val encoder = UnsafeJsonEncoder.keyValueEncoderFor(schemaStr, keyColumns)
+      val start = System.currentTimeMillis()
+      var i = 0;
+      while (i < 10) {
+        val (keyByteArray, valByteArray) = encoder(json.getBytes())
+        val keyUnsafeRow = UnsafeRow.readExternal(keyByteArray)
+        val valueUnsafeRow = UnsafeRow.readExternal(valByteArray)
+        assert(keyUnsafeRow.getInt(0) == 10)
+        assert(valueUnsafeRow.getString(0) == "10")
+        assert(valueUnsafeRow.getLong(1) == 100L)
+        i += 1
+      }
+      val end = System.currentTimeMillis()
+    }
+
+
+    "Encode json correctly with keys switched" in {
+      val schemaStr = "stringVal string, intVal int, longVal bigint"
+      val keyColumns = Array("intVal")
+      val json = """{"intVal": 10, "stringVal": "10", "longVal" : 100 }"""
+      val encoder = UnsafeJsonEncoder.keyValueEncoderFor(schemaStr, keyColumns)
+      val start = System.currentTimeMillis()
+      var i = 0;
+      while (i < 10) {
+        val (keyByteArray, valByteArray) = encoder(json.getBytes())
+        val keyUnsafeRow = UnsafeRow.readExternal(keyByteArray)
+        val valueUnsafeRow = UnsafeRow.readExternal(valByteArray)
+        assert(keyUnsafeRow.getInt(0) == 10)
+        assert(valueUnsafeRow.getString(0) == "10")
+        assert(valueUnsafeRow.getLong(1) == 100L)
+        i += 1
+      }
+      val end = System.currentTimeMillis()
+    }
+
+    "Encode json correctly with no keys " in {
+      val schemaStr = "stringVal string, intVal int, longVal bigint"
+      val keyColumns = Array[String]()
+      val json = """{"intVal": 10, "stringVal": "10", "longVal" : 100 }"""
+      val encoder = UnsafeJsonEncoder.keyValueEncoderFor(schemaStr, keyColumns)
+      val start = System.currentTimeMillis()
+      var i = 0;
+      while (i < 10) {
+        val (keyByteArray, valByteArray) = encoder(json.getBytes())
+        val valueUnsafeRow = UnsafeRow.readExternal(valByteArray)
+        assert(keyByteArray == null)
+        assert(valueUnsafeRow.getString(0) == "10")
+        assert(valueUnsafeRow.getInt(1) == 10)
+        assert(valueUnsafeRow.getLong(2) == 100L)
+        i += 1
+      }
+      val end = System.currentTimeMillis()
+    }
+
+    "Encode json correctly with keys but null key value " in {
+      val schemaStr = "stringVal string, intVal int, longVal bigint"
+      val keyColumns = Array[String]("intVal")
+      val json = """{"stringVal": "10", "longVal" : 100 }"""
+      val encoder = UnsafeJsonEncoder.keyValueEncoderFor(schemaStr, keyColumns)
+      val start = System.currentTimeMillis()
+      var i = 0;
+      while (i < 10) {
+        val (keyByteArray, valByteArray) = encoder(json.getBytes())
+        val valueUnsafeRow = UnsafeRow.readExternal(valByteArray)
+        val keyUnsafeRow = UnsafeRow.readExternal(keyByteArray)
+        assert(keyUnsafeRow.isNullAt(0))
+        assert(valueUnsafeRow.getString(0) == "10")
+        assert(valueUnsafeRow.getLong(1) == 100L)
+        i += 1
+      }
+      val end = System.currentTimeMillis()
+    }
+
+
+    "Encode json correctly with null values " in {
+      val schemaStr = "stringVal string, intVal int, longVal bigint"
+      val keyColumns = Array("intVal", "stringVal")
+      val json = """{"intVal": 10, "stringVal": "10" }"""
+      val encoder = UnsafeJsonEncoder.keyValueEncoderFor(schemaStr, keyColumns)
+      val start = System.currentTimeMillis()
+      var i = 0;
+      while (i < 10) {
+        val (keyByteArray, valByteArray) = encoder(json.getBytes())
+        val keyUnsafeRow = UnsafeRow.readExternal(keyByteArray)
+        assert(valByteArray === null)
+        assert(keyUnsafeRow.getString(1) == "10")
+        assert(keyUnsafeRow.getInt(0) == 10)
+        i += 1
       }
       val end = System.currentTimeMillis()
     }
