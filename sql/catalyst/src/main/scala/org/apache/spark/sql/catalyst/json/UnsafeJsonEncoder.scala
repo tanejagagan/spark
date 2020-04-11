4f51a9850f85280c50ebdc0c16e9e407e844cce1
Gagan Personal <gagan-personal@Gagans-MacBook-Pro.local>
2020-04-11 00:28:55 -0700

    Unsafe json encoder refactoring

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/json/UnsafeJsonEncoder.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/json/UnsafeJsonEncoder.scala
@@ -17,21 +17,329 @@
 package org.apache.spark.sql.catalyst.json
 
 import java.io.{ByteArrayOutputStream, OutputStreamWriter}
-import java.util.{Map => JMap}
+
+import scala.collection.mutable
 
 import com.fasterxml.jackson.core.{JsonFactory, JsonParser}
 
 import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.catalyst.expressions.{AttributeReference, UnsafeProjection, UnsafeRow}
-import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.catalyst.analysis.{FunctionRegistry, UnresolvedAttribute, UnresolvedFunction}
+import org.apache.spark.sql.catalyst.expressions.{Alias, AttributeReference, Expression, InterpretedProjection, UnsafeProjection, UnsafeRow}
+import org.apache.spark.sql.catalyst.parser.CatalystSqlParser
+import org.apache.spark.sql.types.{StructField, StructType}
 import org.apache.spark.unsafe.types.UTF8String
+
+
 
 
 object UnsafeJsonEncoder {
 
-  private class Encoder(schemaStr : String,
-                jParser : JacksonParser,
-                unsafeProjection: UnsafeProjection) extends (Array[Byte] => Array[Byte]) {
+  val functionRegistry = FunctionRegistry.builtin
+
+
+  def encoderFor(inputSchema : String): (Array[Byte] => Array[Byte]) = {
+    return new Encoder(inputSchema);
+  }
+
+
+
+  def encoderBuilder(inputSchema : String) : EncoderBuilder = {
+    return new EncoderBuilder(inputSchema)
+  }
+
+  def keyValueEncoderBuilder(inputSchema : String) : KeyValueEncoderBuilder = {
+    return new KeyValueEncoderBuilder(inputSchema)
+  }
+
+  abstract class Builder[T, R] (val inputSchema : String) {
+
+    this: T =>
+
+    protected var expressions : Array[String] = _
+    protected var options : mutable.Map[String, String] = mutable.Map[String, String]()
+
+    def option(key : String, value : String): T = {
+      options.put(key, value)
+      return this
+    }
+
+    def options(map : Map[String, String]) : T = {
+      map.foreach( e => options.put(e._1, e._2))
+      return this
+    }
+
+    def select(col : String*): T = {
+      expressions = col.toArray
+      return this;
+    }
+
+    def build(): R
+
+  }
+
+  class EncoderBuilder( inputSchema : String)
+    extends Builder[EncoderBuilder, (Array[Byte] => Array[Byte])](inputSchema) {
+    def build(): (Array[Byte] => Array[Byte]) = {
+      if (expressions != null) {
+        return new EncoderWithExpression(inputSchema, expressions)
+      } else {
+        return new Encoder(inputSchema)
+      }
+    }
+  }
+
+  class KeyValueEncoderBuilder(inputSchema : String)
+    extends Builder[KeyValueEncoderBuilder,
+      (Array[Byte] => (Array[Byte], Array[Byte]))](inputSchema) {
+    protected var keyColumns : Array[String] = _
+
+     def keyColumns(col : String*) : KeyValueEncoderBuilder = {
+      keyColumns = col.toArray
+      return this
+    }
+
+    def build(): (Array[Byte] => (Array[Byte], Array[Byte])) = {
+      if (expressions != null) {
+        return new KeyValueEncoderWithExpression(inputSchema, keyColumns, expressions)
+      } else {
+        return new KeyValueEncoder(inputSchema, keyColumns)
+      }
+    }
+  }
+
+  /*
+  class EncoderBuilder(val inputSchema : String) {
+
+    protected var expressions : Array[String] =_
+    protected var options : mutable.Map[String, String]  = mutable.Map[String, String]()
+
+    def option(key : String, value : String): EncoderBuilder = {
+      options.put(key, value)
+      return this
+    }
+
+    def options(map : Map[String, String]) : EncoderBuilder = {
+      map.foreach( e => options.put(e._1, e._2))
+      return this
+    }
+
+    def select(col : String*): EncoderBuilder ={
+      expressions = col.toArray
+      return this;
+    }
+
+    def build(): (Array[Byte] => Array[Byte]) = {
+      if (expressions != null) {
+        return new EncoderWithExpression(inputSchema, expressions)
+      } else {
+        return new Encoder(inputSchema)
+      }
+    }
+  }
+
+  class KeyValueEncoderBuilder(val inputSchema : String) {
+
+    protected var expressions : Array[String] =_
+    protected var keyColumns : Array[String] =_
+    protected var options : mutable.Map[String, String]  = mutable.Map[String, String]()
+
+    def option(key : String, value : String): KeyValueEncoderBuilder  = {
+      options.put(key, value)
+      return this
+    }
+
+    def options(map : Map[String, String]) : KeyValueEncoderBuilder = {
+      map.foreach( e => options.put(e._1, e._2))
+      return this
+    }
+
+    def select(col : String*): KeyValueEncoderBuilder ={
+      expressions = col.toArray
+      return this;
+    }
+
+    def keyColumns(col : String*) : KeyValueEncoderBuilder = {
+      keyColumns = col.toArray
+      return this
+    }
+
+    def build(): (Array[Byte] => (Array[Byte], Array[Byte])) = {
+      if (expressions != null) {
+        return new KeyValueEncoderWithExpression(inputSchema, keyColumns, expressions)
+      } else {
+        return new KeyValueEncoder(inputSchema, keyColumns)
+      }
+    }
+  }
+
+   */
+
+
+  def encoderFor(inputSchema : String,
+                            expressions : Array[String]) : (Array[Byte] => Array[Byte]) = {
+    new EncoderWithExpression(inputSchema, expressions)
+  }
+
+  def keyValueEncoderFor(inputSchema : String,
+                         keyColumns : Array[String] ) : Array[Byte] =>
+    (Array[Byte], Array[Byte]) = {
+    return new KeyValueEncoder(inputSchema, keyColumns)
+  }
+
+  def keyValueEncoderFor(inputSchema : String,
+                  keyColumns : Array[String],
+                  expressions : Array[String]) : (Array[Byte] => (Array[Byte], Array[Byte])) = {
+    new KeyValueEncoderWithExpression(inputSchema, keyColumns, expressions)
+  }
+
+  def decoderFor( schemaStr : String ) : InternalRow => Array[Byte] = {
+    val schema = StructType.fromDDL(schemaStr)
+    val fn: InternalRow => Array[Byte] = { row =>
+      val byteBufferStream = new ByteArrayOutputStream()
+      val outputStreamWriter = new OutputStreamWriter(byteBufferStream);
+      val generator = new JacksonGenerator(schema, outputStreamWriter,
+        new JSONOptions(Map(), "UTC"))
+      generator.write(row)
+      generator.flush()
+      byteBufferStream.toByteArray
+    }
+    fn
+  }
+
+
+
+  private class KeyValueEncoderWithExpression(inputSchema : String,
+                                              keyColumns : Array[String],
+                                              expressions : Array[String] )
+    extends (Array[Byte] => (Array[Byte], Array[Byte])) {
+    val unresolvedExpressions = expressions.map(CatalystSqlParser.parseExpression(_))
+    val schema = StructType.fromDDL(inputSchema)
+    val inputAttributes = schema.fields.map(x => AttributeReference(x.name, x.dataType)()).toSeq
+    val attributeMap = inputAttributes.map(a => a.name -> a).toMap
+    val resolvedAttributes = unresolvedExpressions.map(e => resolveExpression(e, attributeMap))
+    val resolvedAttributeMap = resolvedAttributes.map{ a => getAttributeName(a)->a}.toMap
+
+
+    val jParser = new JacksonParser(schema, new JSONOptions(Map(), "UTC"))
+    val projection = new InterpretedProjection(resolvedAttributes, inputAttributes)
+
+    val outputSchemaVal = new StructType(
+      resolvedAttributes.map { e => StructField(
+        getAttributeName(e), e.dataType) }.toArray)
+    // val unsafeProjection = UnsafeProjection.create(outputSchemaVal)
+    val outputSchemaMap = outputSchemaVal.fields.map( f => f.name -> f).toMap
+
+
+    val valueSchemaFields = outputSchemaVal.fields.filterNot(c => keyColumns.contains(c.name))
+    val keyAttributes : Seq[Expression] =
+      keyColumns.map( c => resolvedAttributeMap(c)).toSeq
+    val valueAttributes : Seq[Expression] =
+      valueSchemaFields.map( f => resolvedAttributeMap(f.name))
+
+    val keyUnsafeProjection = if (keyAttributes.nonEmpty) {
+      Some(UnsafeProjection.create(keyAttributes, inputAttributes))
+    } else {
+      None
+    }
+    val valueUnsafeProjection = if (valueAttributes.nonEmpty) {
+      Some(UnsafeProjection.create(valueAttributes, inputAttributes))
+    } else {
+      None
+    }
+
+
+    override def apply(bytes: Array[Byte]): (Array[Byte], Array[Byte]) = {
+      val rows = jParser.parse[Array[Byte]](bytes, byteArrayParser, b => UTF8String.fromBytes(b) )
+      val kRow = extractRow(rows, keyUnsafeProjection, false)
+      val vRow = extractRow(rows, valueUnsafeProjection, true)
+      (kRow, vRow)
+    }
+
+    def getAttributeName(expr : Expression ): String = {
+      expr match {
+        case Alias(expr, name) => name
+        case AttributeReference(name, _, _, _) => name
+        case  _ => expr.prettyName
+      }
+    }
+
+
+    // If all Values are null then value should be set to null
+    // This will have kafka reclaim during compaction
+    private def extractRow(rows: Seq[InternalRow],
+                           oProjection: Option[UnsafeProjection],
+                           overrideAllNull : Boolean ): Array[Byte] = {
+      oProjection.map { projection =>
+        val rRow = projection.apply(rows.head)
+        if(overrideAllNull && allNull(rRow)) {
+          null
+        } else {
+          UnsafeRow.writeExternal(rRow)
+        }
+      }.getOrElse(null)
+    }
+
+    private def allNull(row: UnsafeRow): Boolean = {
+      if (!row.anyNull()) {
+        return false
+      }
+      var i = 0
+      val fields = row.numFields()
+      while (i < fields) {
+        if (!row.isNullAt(i)) {
+          return false
+        }
+        i += 1
+      }
+      return true
+    }
+
+    override def toString(): String = {
+      val params = jParser.options.parameters.mkString(",")
+      val columns = keyColumns.map(c => s"$c").mkString(",")
+      s"""{"schemaStr" : "$inputSchema", "keyColumns" : [${columns.mkString(",")}],
+         | "parserOptions" : "$params"}""".stripMargin
+    }
+  }
+
+
+
+
+  private class EncoderWithExpression( inputSchema : String,
+                                expressions : Array[String])
+         extends (Array[Byte] => Array[Byte]) {
+     val unresolvedExpressions = expressions.map(CatalystSqlParser.parseExpression(_))
+     val schema = StructType.fromDDL(inputSchema)
+     val inputAttributes = schema.fields.map(x => AttributeReference(x.name, x.dataType)()).toSeq
+     val attributeMap = inputAttributes.map(a => a.name -> a).toMap
+     val resolvedAttributes = unresolvedExpressions.map(e => resolveExpression(e, attributeMap))
+     val jParser = new JacksonParser(schema, new JSONOptions(Map(), "UTC"))
+     val projection = new InterpretedProjection(resolvedAttributes, inputAttributes)
+
+     val outputSchemaVal = new StructType(
+       resolvedAttributes.map { e => StructField(e.prettyName, e.dataType) }.toArray)
+
+     def outputSchema: StructType = outputSchemaVal
+
+     val unsafeProjection = UnsafeProjection.create(outputSchemaVal)
+
+     override def apply(bytes: Array[Byte]): Array[Byte] = {
+       val rows = jParser.parse[Array[Byte]](bytes, byteArrayParser, b => UTF8String.fromBytes(b))
+       val projectedRow = projection.apply(rows.head)
+       val uRow = unsafeProjection.apply(projectedRow)
+       UnsafeRow.writeExternal(uRow)
+     }
+  }
+
+  private class Encoder(schemaStr : String
+                // jParser : JacksonParser,
+                // unsafeProjection: UnsafeProjection
+                       ) extends (Array[Byte] => Array[Byte]) {
+
+    val schema = StructType.fromDDL(schemaStr)
+    val jParser = new JacksonParser(schema, new JSONOptions(Map(), "UTC"))
+    val unsafeProjection = UnsafeProjection.create(schema)
+
     override def apply(bytes: Array[Byte]): Array[Byte] = {
       val rows = jParser.parse[Array[Byte]](bytes, byteArrayParser, b => UTF8String.fromBytes(b) )
       val uRow = unsafeProjection.apply(rows.head)
@@ -45,11 +353,30 @@ object UnsafeJsonEncoder {
   }
 
   private class KeyValueEncoder(schemaStr : String,
-                        keyColumns : Seq[String],
-                        jParser: JacksonParser,
-                        keyUnsafeProjection: Option[UnsafeProjection],
-                        valueUnsafeProjection : Option[UnsafeProjection])
+                        keyColumns : Seq[String]
+                               )
+                        // jParser: JacksonParser,
+                        // keyUnsafeProjection: Option[UnsafeProjection],
+                        // valueUnsafeProjection : Option[UnsafeProjection])
     extends (Array[Byte] => (Array[Byte], Array[Byte])) {
+
+    val schema = StructType.fromDDL(schemaStr)
+    val jParser = new JacksonParser(schema, new JSONOptions(Map(), "UTC"))
+    val inputSchemaAttributes = schema.fields.map(f => new AttributeReference(f.name, f.dataType)())
+    val inputSchemaAttributesMap = inputSchemaAttributes.map( a => a.name -> a).toMap
+    val valueSchemaFields = schema.fields.filterNot(c => keyColumns.contains(c.name))
+    val keyAttributes = keyColumns.map( c => inputSchemaAttributesMap(c))
+    val valueAttributes = valueSchemaFields.map( f => inputSchemaAttributesMap(f.name))
+    val keyUnsafeProjection = if (keyAttributes.nonEmpty) {
+      Some(UnsafeProjection.create(keyAttributes, inputSchemaAttributes))
+    } else {
+      None
+    }
+    val valueUnsafeProjection = if (valueAttributes.nonEmpty) {
+      Some(UnsafeProjection.create(valueAttributes, inputSchemaAttributes))
+    } else {
+      None
+    }
 
     override def apply(bytes: Array[Byte]): (Array[Byte], Array[Byte]) = {
       val rows = jParser.parse[Array[Byte]](bytes, byteArrayParser, b => UTF8String.fromBytes(b) )
@@ -97,30 +424,33 @@ object UnsafeJsonEncoder {
     }
   }
 
+  /*
+
   val encoderFor : String => Array[Byte] => Array[Byte] = { schemaStr =>
-    val schema = StructType.fromDDL(schemaStr)
-    val jParser = new JacksonParser(schema, new JSONOptions(Map(), "UTC"))
-    val unsafeProjection = UnsafeProjection.create(schema)
-    new Encoder(schemaStr, jParser, unsafeProjection)
+    // val schema = StructType.fromDDL(schemaStr)
+    // val jParser = new JacksonParser(schema, new JSONOptions(Map(), "UTC"))
+    // val unsafeProjection = UnsafeProjection.create(schema)
+    new Encoder(schemaStr) // , jParser, unsafeProjection)
   }
 
   val decoderFor : String =>
     InternalRow => Array[Byte] = { schemaStr =>
     val schema = StructType.fromDDL(schemaStr)
     val fn: InternalRow => Array[Byte] = { row =>
-      val stream = new ByteArrayOutputStream()
-      val writer =
-        new OutputStreamWriter(stream)
-      val generator = new JacksonGenerator(schema, writer,
+      val byteBufferStream = new ByteArrayOutputStream()
+      val outputStreamWriter = new OutputStreamWriter(byteBufferStream);
+      val generator = new JacksonGenerator(schema, outputStreamWriter,
         new JSONOptions(Map(), "UTC"))
       generator.write(row)
-      stream.toByteArray
+      generator.flush()
+      byteBufferStream.toByteArray
     }
     fn
   }
 
   val keyValueEncoderFor : (String,
     Array[String]) => Array[Byte] => (Array[Byte], Array[Byte]) = { (schemaStr, keyColumns) =>
+    /*
     val schema = StructType.fromDDL(schemaStr)
     val jParser = new JacksonParser(schema, new JSONOptions(Map(), "UTC"))
     val inputSchemaAttributes = schema.fields.map(f => new AttributeReference(f.name, f.dataType)())
@@ -138,10 +468,24 @@ object UnsafeJsonEncoder {
     } else {
       None
     }
+
+
     new KeyValueEncoder(schemaStr, keyColumns, jParser, keyUnsafeProjection, valueUnsafeProjection)
+
+     */
+    new KeyValueEncoder(schemaStr, keyColumns)
   }
+
+   */
 
   def byteArrayParser(jsonFactory: JsonFactory, record: Array[Byte]): JsonParser = {
     jsonFactory.createParser(record, 0, record.length)
   }
+
+  private def resolveExpression(e: Expression,
+                                attributeMap: Map[String, AttributeReference]): Expression =
+    e.transform {
+      case a: UnresolvedAttribute if a.nameParts.size == 1 => attributeMap(a.nameParts.head)
+      case fn : UnresolvedFunction => functionRegistry.lookupFunction(fn.name, fn.children )
+    }
 }
--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/json/UnsafeJsonEncoderSpec.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/json/UnsafeJsonEncoderSpec.scala
@@ -17,8 +17,9 @@
 
 package org.apache.spark.sql.catalyst.json
 
+import scala.reflect.runtime.universe._
 import org.scalatest.WordSpec
-
+import org.apache.spark.sql.catalyst.encoders.{ExpressionEncoder, UnsafeEncoder}
 import org.apache.spark.sql.catalyst.expressions.UnsafeRow
 
 
@@ -32,14 +33,19 @@ class UnsafeJsonEncoderSpec extends WordSpec {
       val start = System.currentTimeMillis()
       var i = 0;
       while (i < 10) {
+        // Test Encoder
         val byteArray = encoder(json.getBytes())
         val unsafeRow = UnsafeRow.readExternal(byteArray)
-        assert(unsafeRow.getInt(0) == 10)
-        assert(unsafeRow.getString(1) == "10")
-        assert(unsafeRow.getLong(2) == 100L)
+        assert(unsafeRow.getInt(0) === 10)
+        assert(unsafeRow.getString(1) === "10")
+        assert(unsafeRow.getLong(2) === 100L)
+
+        // Test Decoder
+        val decoded = UnsafeJsonEncoder.decoderFor(schemaStr).apply(unsafeRow)
+        val str = new String(decoded)
+        assert(str.length > 0)
         i += 1
       }
-      val end = System.currentTimeMillis()
     }
 
     "Encode json correctly with keys" in {
@@ -137,5 +143,142 @@ class UnsafeJsonEncoderSpec extends WordSpec {
       }
       val end = System.currentTimeMillis()
     }
+
+    /*
+    "Encode with expression" in {
+      val schemaStr = "stringVal string, intVal int, longVal bigint"
+      val keyColumns = Array("intVal", "stringVal")
+      val json = """{"intVal": 10, "stringVal": "10" }"""
+      val expressions = Array("stringVal", "cast(intVal as string)")
+      val expressionEncoder = UnsafeJsonEncoder.encoderForSchemaAndExpression(schemaStr,
+        expressions)
+      val bytes = expressionEncoder.apply(json.getBytes)
+      val uRow = UnsafeRow.readExternal(bytes);
+      assert( uRow.getString(0) == "10")
+      assert( uRow.getString(1) == "10")
+    }
+
+    "KeyValue Encode with expression" in {
+      val schemaStr = "stringVal string, intVal int, longVal bigint"
+      val keyColumns = Array("intVal", "stringVal")
+      val json = """{"intVal": 10, "stringVal": "10", "longVal": 100 }"""
+      val expressions = Array("stringVal", "cast(intVal as string) as intVal",
+        "cast(longVal as string) as longVal1",
+        "(longVal + intVal) as addVal",
+        "(longVal - intVal) as subVal",
+        "sha2(stringVal, 256) as hash"
+        )
+      val expressionEncoder = UnsafeJsonEncoder.keyValueEncoderForSchemaAndExpression(
+        schemaStr, keyColumns, expressions
+      )
+      val (kBytes, valBytes) = expressionEncoder.apply(json.getBytes)
+      val kRow = UnsafeRow.readExternal(kBytes);
+      val vRow = UnsafeRow.readExternal(valBytes);
+      assert( kRow.getString(0) == "10")
+      assert( kRow.getString(1) == "10")
+      assert("100" == vRow.getString(0))
+      assert(110 == vRow.getLong(1))
+      assert(90 === vRow.getLong(2))
+      assert("4a44dc15364204a80fe80e9039455cc1608281820fe2b24f1e5233ade6af1dd5" ==
+        vRow.getString(3))
+    }
+
+     */
+
+    "KeyValue Encode with expression with builder" in {
+      val schemaStr = "stringVal string, intVal int, longVal bigint"
+      val keyColumns = Array("intVal", "stringVal")
+      val json = """{"intVal": 10, "stringVal": "10", "longVal": 100 }"""
+      val expressions = Array("stringVal", "cast(intVal as string) as intVal",
+        "cast(longVal as string) as longVal1",
+        "(longVal + intVal) as addVal",
+        "(longVal - intVal) as subVal",
+        "sha2(stringVal, 256) as hash"
+      )
+      val builder = UnsafeJsonEncoder.keyValueEncoderBuilder(schemaStr)
+      val encoder = builder.keyColumns(keyColumns: _*)
+        .select(expressions : _*)
+        .build()
+      val (kBytes, valBytes) = encoder.apply(json.getBytes)
+      val kRow = UnsafeRow.readExternal(kBytes);
+      val vRow = UnsafeRow.readExternal(valBytes);
+      test(("10", "10"), kRow)
+      test(("100", 110, 90, "4a44dc15364204a80fe80e9039455cc1608281820fe2b24f1e5233ade6af1dd5"),
+        vRow)
+    }
+  }
+
+  "KeyValue Encoder with expression with builder" in {
+    val schemaStr = "stringVal string, intVal int, longVal bigint"
+    val keyColumns = Array("intVal", "stringVal")
+    val json = """{"intVal": 10, "stringVal": "10", "longVal": 100 }"""
+    val expressions = Array("stringVal",
+      "cast(intVal as string) as intVal",
+      "cast(longVal as string) as longVal1",
+      "(longVal + intVal) as addVal",
+      "(longVal - intVal) as subVal",
+      "sha2(stringVal, 256) as hash"
+    )
+    val encoder = UnsafeEncoder.forSchema(schemaStr)
+      .select(expressions : _*)
+      .forKeys(keyColumns)
+      .build()
+    val (kBytes, valBytes) = encoder.apply(json.getBytes)
+    val kRow = UnsafeRow.readExternal(kBytes);
+    val vRow = UnsafeRow.readExternal(valBytes);
+    test(("10", "10"), kRow)
+    test(("100", 110, 90, "4a44dc15364204a80fe80e9039455cc1608281820fe2b24f1e5233ade6af1dd5"), vRow)
+  }
+
+  "KeyValue Encoder with no expression" in {
+    val schemaStr = "stringVal string, intVal int, longVal bigint"
+    val keyColumns = Array("intVal", "stringVal")
+    val json = """{"intVal": 10, "stringVal": "10", "longVal": 100 }"""
+    val encoder = UnsafeEncoder.forSchema(schemaStr).forKeys(keyColumns)
+      .build()
+    val (kBytes, valBytes) = encoder.apply(json.getBytes)
+    val kRow = UnsafeRow.readExternal(kBytes);
+    val vRow = UnsafeRow.readExternal(valBytes);
+    test((10, "10"), kRow)
+    test((100L), vRow)
+  }
+
+  "Value Encoder with expression with builder" in {
+    val schemaStr = "stringVal string, intVal int, longVal bigint"
+    // val keyColumns = Array("intVal", "stringVal")
+    val json = """{"intVal": 10, "stringVal": "10", "longVal": 100 }"""
+    val expressions = Array("stringVal",
+      "cast(intVal as string) as intValStr",
+      "cast(longVal as string) as longVal1",
+      "(longVal + cast(intVal as bigint)) as addVal",
+      "(longVal - cast(intVal as bigint)) as subVal",
+      "sha2(stringVal, 256) as hash"
+    )
+    val encoder = UnsafeEncoder.forSchema(schemaStr)
+      .select(expressions : _*)
+      .build()
+    val bytes = encoder.apply(json.getBytes)
+    val row = UnsafeRow.readExternal(bytes);
+    test(("10", "10", "100", 110L, 90L,
+      "4a44dc15364204a80fe80e9039455cc1608281820fe2b24f1e5233ade6af1dd5"), row)
+  }
+
+  "Value Encoder with  with builder" in {
+    val schemaStr = "stringVal string, intVal int, longVal bigint"
+    // val keyColumns = Array("intVal", "stringVal")
+    val json = """{"intVal": 10, "stringVal": "10", "longVal": 100 }"""
+
+    val encoder = UnsafeEncoder.forSchema(schemaStr)
+      .build()
+    val bytes = encoder.apply(json.getBytes)
+    val row = UnsafeRow.readExternal(bytes);
+    test(("10", 10, 100L), row)
+  }
+
+  private def test[T]( expected : T, row : UnsafeRow)(implicit tag: TypeTag[T]): Unit = {
+    val encoder = ExpressionEncoder[T]()
+    val convertBack = encoder.resolveAndBind().fromRow(row)
+    assert(expected === convertBack)
   }
 }
+
